d042f2e1c201aeb43c97f0e4327b06a00db70f5a
Dylan McKee <dylan@djmckee.co.uk>
2015-11-26 20:56:26 +0000

    Key up/down differentiated

--- app/src/main/java/com/projectcrescendo/pianotest/MainActivity.java
+++ app/src/main/java/com/projectcrescendo/pianotest/MainActivity.java
@@ -3,6 +3,7 @@ package com.projectcrescendo.pianotest;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.RelativeLayout;
 
 import com.twobard.pianoview.Piano;
@@ -19,7 +20,13 @@ public class MainActivity extends ActionBarActivity {
         piano.setPianoKeyListener(new Piano.PianoKeyListener() {
             @Override
             public void keyPressed(int id, int action) {
-                Log.d("Piano key pressed", "key number " + id + " pressed!");
+                if (action == MotionEvent.ACTION_DOWN) {
+                    Log.d("Piano key pressed", "key number " + id + " pressed down");
+
+                } else if (action == MotionEvent.ACTION_UP) {
+                    Log.d("Piano key pressed", "key number " + id + " released");
+
+                }
             }
         });
         mainContent.addView(piano);
